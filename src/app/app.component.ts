c849f1dbc0c883d9df32c20d3e1ee3b12135c5e4
Dmitry Komkov <drnecromant@gmail.com>
2018-03-03 11:48:55 +0100

    Update project for new ng with all tests and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ArtKomkova';
+  title = 'Art Komkova!!!';
 }
