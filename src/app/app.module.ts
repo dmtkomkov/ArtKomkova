0d6a97393078637f779d6e6bf882c51dd44e5ea6
Dmitry Komkov <drnecromant@gmail.com>
2018-03-07 20:44:03 +0100

    Made logo clickable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
