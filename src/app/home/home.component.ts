26e56264693b583b8ad7280a8d0e635ed8408c44
Dmitry Komkov <drnecromant@gmail.com>
2018-03-13 20:03:31 +0100

    Added empty test coollections that refer to collection url

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  collections: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.getCollections().subscribe(
+      collections => {
+        this.collections = collections;
+      }
+    );
   }
 
+  getCollections(): string[] {
+    return Observable.of(
+      [
+        { 'name': 'collection1', 'cover': '19913' },
+        { 'name': 'collection2', 'cover': '19914' },
+        { 'name': 'collection3', 'cover': '19915' },
+        { 'name': 'collection4', 'cover': '19916' },
+      ]
+    );
+  }
 }
