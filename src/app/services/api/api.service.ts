d1f893e297515a39edb4ee5b305d0dcbb9917c6c
Dmitry Komkov <drnecromant@gmail.com>
2018-03-28 11:17:01 +0200

    add getFolderItems to api service

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { environment } from '../../../environments/environment';
+import { Item } from '../../interfaces';
 
 @Injectable()
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
+  getFolderItems(folderId: string, mimeType: string = null): Observable<Item[]> {
+    let query: string = `"${folderId}"+in+parents`
+    if (mimeType != null) query += `+and+mimeType+=+"${mimeType}"`
+    let params: HttpParams = new HttpParams({
+      fromObject: {
+        'q': query,
+        'key': environment.apiKey,
+        'fields': 'files(mimeType,name,id,webContentLink)',
+      }
+    });
+    return this.http.get(environment.apiUrl, { params: params }).map(res => res.files);
+  }
 }
